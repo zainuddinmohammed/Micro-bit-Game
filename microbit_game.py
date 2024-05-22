32824d114b5ae88df41d53402cb5875bf477b8e6
zainuddinmohammed <160622570+zainuddinmohammed@users.noreply.github.com>
2024-05-21 23:00:34 -0500

    microbit_game.py

--- microbit_game.py
+++ microbit_game.py
@@ -18,74 +18,147 @@ def reset_board():
 position = 2
 rand = random.randint(0, 4)
 stone = -1
+page = 0
 
 display.set_pixel(position, 4, 9)
 
 # Loop
+
+display.show(Image.HEART)
+time.sleep(1)
+
 while True:
 
-    if stone < 5:
-        stone = stone + 1
-        if stone == 5:
-            
-            if rand == position:
-                display.show(Image.SAD)
-                display.scroll('Time:')
-                display.scroll((running_time() - 5000)/1000)
-                display.scroll('Try Again!')
-                display.show(Image.CLOCK12)
-                time.sleep(0.1)
-                display.show(Image.CLOCK1)
-                time.sleep(0.05)
-                display.show(Image.CLOCK2)
-                time.sleep(0.05)
-                display.show(Image.CLOCK3)
-                time.sleep(0.05)
-                display.show(Image.CLOCK4)
-                time.sleep(0.05)
-                display.show(Image.CLOCK5)
-                time.sleep(0.05)
-                display.show(Image.CLOCK6)
-                time.sleep(0.05)
-                display.show(Image.CLOCK7)
-                time.sleep(0.05)
-                display.show(Image.CLOCK8)
-                time.sleep(0.05)
-                display.show(Image.CLOCK9)
-                time.sleep(0.05)
-                display.show(Image.CLOCK10)
-                time.sleep(0.05)
-                display.show(Image.CLOCK11)
-                time.sleep(0.05)
-                display.show(Image.CLOCK12)
-                time.sleep(0.5)
-                reset_board()
-                sys.exit()
-            display.set_pixel(rand, stone - 1, 0)
-            stone = -1
-            rand = random.randint(0, 4)
-        elif stone == 0:
-            display.set_pixel(rand, stone, 9)
-        elif stone > 0:
-            display.set_pixel(rand, stone - 1, 0)
-            display.set_pixel(rand, stone, 9)
-
-    display.set_pixel(position, 4, 0)
-
-    if button_a.was_pressed():
-        if position > 0:
-            display.set_pixel(position, 4, 0)
-            position = position - 1
-            
+    while page == 0:
         
-    if button_b.was_pressed():
-        if position < 4:
-            display.set_pixel(position, 4, 0)
-            position = position + 1
-
-    display.set_pixel(position, 4, 9)
+        display.show('M')
+        time.sleep(1)
+        display.show(Image('90000:'
+                           '90000:'
+                           '90000:'
+                           '90000:'
+                           '90000'))
+        menu_position = 0
+        menu = True
         
-    time.sleep(0.1)
+        while menu == True:
+            
+            display.set_pixel(2, menu_position, 9)
+            
+            if button_a.was_pressed():
+                if menu_position == 0:
+                    reset_board()
+                    display.show('3')
+                    time.sleep(1)
+                    display.show('2')
+                    time.sleep(1)
+                    display.show('1')
+                    time.sleep(1)
+                    display.show(Image('00000:'
+                                       '00000:'
+                                       '00000:'
+                                       '00000:'
+                                       '00900'))
+                    time.sleep(1)
+                    reset_board()
+                    menu = False
+                    page = 1
+                if menu_position == 1:
+                    reset_board()
+                    menu = False
+                    page = 2
+                    
+                
+            if button_b.was_pressed():
+                if menu_position < 4:
+                    display.set_pixel(2, menu_position, 0)
+                    menu_position = menu_position + 1
+                else:
+                    display.set_pixel(2, menu_position, 0)
+                    menu_position = 0
+            
+
+    while page == 1:
+        if stone < 5:
+            stone = stone + 1
+            if stone == 5:
+                
+                if rand == position:
+                    position = 2
+                    display.show(Image.SAD)
+                    time.sleep(1)
+                    display.show(Image('00000:'
+                                       '90000:'
+                                       '00000:'
+                                       '90000:'
+                                       '00000'))
+                    go_menu = True
+                    go_menupos = 1
+                    while go_menu == True:
+                        display.set_pixel(2, go_menupos, 9)
+                        if button_a.was_pressed():
+                            if go_menupos == 1:
+                                reset_board()
+                                go_menu = False
+                                page = 1
+                            if go_menupos == 3:
+                                reset_board()
+                                go_menu = False
+                                page = 0
+                                
+                            
+                        if button_b.was_pressed():
+                            if go_menupos == 1:
+                                display.set_pixel(2, go_menupos, 0)
+                                go_menupos = 3
+                            else:
+                                display.set_pixel(2, go_menupos, 0)
+                                go_menupos = 1
+                display.set_pixel(rand, stone - 1, 0)
+                stone = -1
+                rand = random.randint(0, 4)
+            elif stone == 0:
+                display.set_pixel(rand, stone, 9)
+            elif stone > 0:
+                display.set_pixel(rand, stone - 1, 0)
+                display.set_pixel(rand, stone, 9)
+    
+        display.set_pixel(position, 4, 0)
+    
+        if button_a.was_pressed():
+            if position > 0:
+                display.set_pixel(position, 4, 0)
+                position = position - 1
+                
+            
+        if button_b.was_pressed():
+            if position < 4:
+                display.set_pixel(position, 4, 0)
+                position = position + 1
+    
+        display.set_pixel(position, 4, 9)
+            
+        time.sleep(0.1)
+
+    while page == 2:
+        if(compass.is_calibrated() == False):
+            compass.calibrate()
+        oncompass = True
+        while(compass.is_calibrated() and oncompass):
+            if(compass.heading() >= 315 or compass.heading() < 45):
+                display.show('N')
+            elif(compass.heading() >= 45 and compass.heading() < 135):
+                display.show('E')
+            elif(compass.heading() >= 135 and compass.heading() < 225):
+                display.show('S')
+            elif(compass.heading() >= 225 and compass.heading() < 315):
+                display.show('W')
+            if(button_a.was_pressed() or button_b.was_pressed()):
+                oncompass = False
+                page = 0
+            
+                
+        
         
     
     
